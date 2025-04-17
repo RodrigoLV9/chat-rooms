377b1a9a10ea9bcefb7ccd971cee3c4061c1654b
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-04-17 15:42:37 -0300

    feat(Controls): Create basic structure for Controls component

--- src/components/Header/Controls.tsx
+++ src/components/Header/Controls.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
-
-export const Controls = () => {
+import React,{useState} from 'react'
+import { FiSun as IconSun } from "react-icons/fi";
+import { FiMoon as IconMoon } from "react-icons/fi";
+export const Controls:React.FC = () => {
+  const [theme,setTheme]=useState<boolean | undefined>(true)
+  const handleTheme=()=>{
+    setTheme(!theme)
+  }
   return (
-    <div>Controls</div>
+    <div className='controls'>
+      <button className="controls__theme" onClick={handleTheme}>
+        {
+          theme ?
+          <IconSun/>
+          :
+          <IconMoon/>
+        }
+      </button>
+      <select className="controls__idiom" name='idiom' id='idiom'>
+        <option value="EN">EN</option>
+        <option value="ES">ES</option>
+      </select>
+    </div>
   )
 }
