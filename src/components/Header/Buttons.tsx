f2c2c7e03d4dea9a4aaae090caf1b39c41dbe695
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-04-17 17:18:25 -0300

    style(Buttons): Add basic style for Buttons component

--- src/components/Header/Buttons.tsx
+++ src/components/Header/Buttons.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Buttons:React.FC = () => {
   return (
     <div className='buttons'>
-      <button>Log In</button>
-      <button>Sign Up</button>
+      <button className='buttons__login'>Log In</button>
+      <button className='buttons__register'>Sign Up</button>
     </div>
   )
 }
