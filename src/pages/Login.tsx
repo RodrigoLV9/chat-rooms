191fb8cae4316366196f63fafa8e0f7cc1e6de92
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-04-18 20:46:26 -0300

    style(Login): Add basic style for Login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import { FaRegEyeSlash as IconEyeDisabled } from "react-icons/fa";
+import { FaRegEye as IconEyeEnabled } from "react-icons/fa";
+import { FcGoogle as IconGoogle } from "react-icons/fc";
 export const Login:React.FC = () => {
   return (
-    <div>Login</div>
+    <main className='pageRegister'>
+      <h1>Log In</h1>
+      <div className='pageRegister__text'>
+        <p>Don't have an account?</p>
+        <Link to="/register">Sign Up</Link>
+      </div>
+      <form action="" className='pageRegister__form'>
+        <div className="containerInput">
+          <label htmlFor="" className='containerInput__label'>Username or Mail*</label>
+          <input type="text" placeholder='Write your username' className='containerInput__input'/>
+        </div>
+        <div className="containerInput">
+          <label htmlFor="" className='containerInput__label'>Password*</label>
+          <div className="containerInput-input">
+            <input type="text" placeholder='Write your password' className='containerInput__input'/>
+            <IconEyeDisabled className='containerInput__icons'/>
+          </div>
+        </div>
+        <div className="form__buttons">
+          <button className='form__buttons-main'>Log In</button>
+          <p>or</p>
+          <button className='form__buttons-google'>
+            <IconGoogle className='form__buttons-google-icon'/>
+            <p>Log In with Google</p>
+          </button>
+        </div>
+      </form>
+    </main>
   )
 }
