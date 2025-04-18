fded832b5de184d3b59031529347bcb39b3359ab
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-04-18 09:09:10 -0300

    feat(Footer): Create structure html for Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer:React.FC = () => {
+  return (
+    <footer className='footer'>
+        <p>© 2025 ChatRooms. All rights reserved.</p>
+        <div className="footerBottom">
+            <a href='#'>Terms of service</a>
+            <a href='#'>Privacy Policy</a>
+            <a href='#'>Contact us</a>
+        </div>
+    </footer>
+  )
+}
