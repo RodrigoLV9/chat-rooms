bc02e4b79278b37984951cc930024fe0f10bb548
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-04-18 08:54:51 -0300

    style(Rooms): Add basic style for Rooms and RoomCard components

--- src/components/Main/RoomCard.tsx
+++ src/components/Main/RoomCard.tsx
@@ -5,16 +5,16 @@ export const RoomCard:React.FC = () => {
   return (
     <div className='roomCard'>
         <div className="roomCard__main">
-            <div className="roomCard__title">
-                <IconWorld/>
+            <div className="roomCard__main-title">
+                <IconWorld className='roomCard-icon'/>
                 <p>Argentina</p>
             </div>
             <p>320 online</p>
         </div>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, unde aperiam minus.</p>
         <button className='roomCard__button'>
-            <IconArrow/>
             <p>Join</p>
+            <IconArrow className='roomCard-icon'/>
         </button>
     </div>
   )
--- src/components/Main/Rooms.tsx
+++ src/components/Main/Rooms.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { RoomCard } from './RoomCard'
-
+import '../../styles/Rooms.css'
 export const Rooms:React.FC = () => {
   return (
-    <section>
+    <section className='rooms'>
         <h2>Chats Rooms</h2>
         <div className="containerRooms">
             <RoomCard/>
