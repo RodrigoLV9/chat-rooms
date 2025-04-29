fa26b2eccaeea282ed5ea43fc87d2c0edc9de34f
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-04-29 00:00:05 -0300

    feat(io): Install socket.io-client and use it for connect it with backend

--- src/components/Chat/ChatBox.tsx
+++ src/components/Chat/ChatBox.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { IoSend as IconSend } from "react-icons/io5";
 import {Message} from './Message.tsx'
+import {io} from 'socket.io-client'
 export const ChatBox:React.FC = () => {
+  const socket=io('http://localhost:3000')
+  useEffect(()=>{
+    socket.on('connect',()=>{
+      console.log('Connected to server')
+    })
+    socket.on('disconnect',()=>{
+      console.log('Disconnected from server')
+    })
+  })
   return (
     <section className='chatBox'>
         <h2 className='chatBox__title'>Chat Argentina</h2>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { UserContext } from './Contexts/UserContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <UserContext>
-      <App />
-    </UserContext>
-  </StrictMode>,
+  <UserContext>
+    <App />
+  </UserContext>
 )
